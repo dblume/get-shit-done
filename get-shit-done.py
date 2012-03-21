df1c8661f7f74dec926d8df6de402de0641f375c
David Blume <david.blume@gmail.com>
2012-03-20 23:37:53 -0700

    Refactored quite a bit and included a few other pull requests.
    * Incompatibilities with Python 2.5 and beyond fixed.
    * Used "with" statement for better file handling.
    * Better MacOS and CygWin compatibility.
    * You no longer have to specify the argument "work" or "play." It'll just toggle modes every time you run it now.
    * When reverting to "play" mode, it retains whatever was in the hosts file after the endToken now.

--- get-shit-done.py
+++ get-shit-done.py
@@ -8,74 +8,90 @@ import os
 def exit_error(error):
     print >> sys.stderr, error
     exit(1)
-    
-iniFile = os.expanduser(path.join("~", ".get-shit-done.ini"))
+
+iniFile = os.path.expanduser(os.path.join("~", ".get-shit-done.ini"))
+
 restartNetworkingCommand = ["/etc/init.d/networking", "restart"]
+restartMacNetworkingCommand = ['ifconfig en0 down; ifconfig en0 up;']
+
+# Windows users may have to set up an exception for write access
+# to the hosts file in Windows Security Essentials.
 hostsFile = '/etc/hosts'
+
 startToken = '## start-gsd'
 endToken = '## end-gsd'
-siteList = ['reddit.com', 'forums.somethingawful.com', 'somethingawful.com',
-            'digg.com', 'break.com', 'news.ycombinator.com', 'infoq.com',
-            'bebo.com', 'twitter.com', 'facebook.com', 'blip.com',
-            'youtube.com', 'vimeo.com', 'delicious.com', 'flickr.com',
-            'friendster.com', 'hi5.com', 'linkedin.com', 'livejournal.com',
-            'meetup.com', 'myspace.com', 'plurk.com', 'stickam.com',
-            'stumbleupon.com', 'yelp.com', 'slashdot.com']
+siteList = ['reddit.com', 'forums.somethingawful.com',
+            'somethingawful.com', 'digg.com', 'break.com',
+            'news.ycombinator.com', 'infoq.com',
+            'bebo.com', 'twitter.com', 'facebook.com',
+            'blip.com', 'youtube.com', 'vimeo.com',
+            'flickr.com', 'friendster.com', 'hi5.com',
+            'linkedin.com', 'livejournal.com',
+            'yourusername.livejournal.com', # TODO use your username
+            'meetup.com', 'myspace.com', 'plurk.com',
+            'stickam.com', 'stumbleupon.com', 'yelp.com',
+            'slashdot.com', 'lifehacker.com',
+            'plus.google.com', 'gizmodo.com',
+            'thedailywtf.com', 'samachar.com', 
+            'hindu.com', 'lifehack.org', 'slickdeals.net',
+            'woot.com']
 
-if os.path.exists(iniFile):
-    iniF = open(iniFile)
-    try:
-        for line in iniF:
-            key, value = map(str.strip, line.split("=", 1))
-            if key == "sites":
-                siteList = [value]
-            elif key == "sites[]":
-                siteList.append(value)
-    finally:
-        iniF.close()
+def loadIniFile():
+    global siteList
+    if os.path.exists(iniFile):
+        with open(iniFile) as iniF:
+            for line in iniF:
+                key, value = map(str.strip, line.split("=", 1))
+                if key == "sites":
+                    siteList = [value]
+                elif key == "sites[]":
+                    siteList.append(value)
 
 def rehash():
-    subprocess.check_call(restartNetworkingCommand)
+    if sys.platform == 'cygwin':
+        return
+    if sys.platform == 'darwin':
+        subprocess.check_call(restartMacNetworkingCommand)
+    else:
+        subprocess.check_call(restartNetworkingCommand)
 
 def work():
-    hFile = open(hostsFile, 'a+')
-    contents = hFile.read()
-
-    if startToken in contents and endToken in contents:
-        exit_error("Work mode already set.")
-
-    print >> hFile, startToken
-
-    for site in siteList:
-        print >> hFile, "127.0.0.1\t" + site
-        print >> hFile, "127.0.0.1\twww." + site
-
-    print >> hFile, endToken
-
+    with open(hostsFile, 'a') as hFile:
+        print >> hFile, startToken
+        for site in siteList:
+            print >> hFile, "127.0.0.1\t" + site
+            if site.count('.') == 1:
+                print >> hFile, "127.0.0.1\twww." + site
+        print >> hFile, endToken
     rehash()
 
-def play():
-    hFile = open(hostsFile, "r+")
-    lines = hFile.readlines()
+def play(startIndex, endIndex, lines):
+    with open(hostsFile, "w") as hFile:
+        hFile.writelines(lines[0:startIndex])
+        hFile.writelines(lines[endIndex+1:])
+    rehash()
 
-    startIndex = -1
-
-    for index, line in enumerate(lines):
-        if line.strip() == startToken:
-            startIndex = index
-
-    if startIndex > -1:
-        lines = lines[0:startIndex]
-
-        hFile.seek(0)
-        hFile.write(''.join(lines))
-        hFile.truncate()
-
-        rehash()
-
-def main():
-    if getpass.getuser() != 'root':
+if __name__ == "__main__":
+    if sys.platform != 'cygwin' and getpass.getuser() != 'root':
         exit_error('Please run script as root.')
-    if len(sys.argv) != 2:
-        exit_error('usage: ' + sys.argv[0] + ' [work|play]')
-    {"work": work, "play": "play"}[sys.argv[1]]()
+
+    loadIniFile()
+
+    # Determine if our siteList is already present.
+    startIndex = -1
+    endIndex = -1
+    with open(hostsFile, "r") as hFile:
+        lines = hFile.readlines()
+        for i, line in enumerate(lines):
+            line = line.strip()
+            if line == startToken:
+                startIndex = i
+            elif line == endToken:
+                endIndex = i
+
+    if startIndex > -1 and endIndex > -1:
+        play(startIndex, endIndex, lines)
+        print "Play mode activated."
+    else:
+        work()
+        print "Get back to work."
